54f526545c35b2f038ffcdba93a1912c7995b612
HenriqueMts <henrique.mts@outlook.com.br>
2022-06-19 21:39:59 -0300

    refactor: using react hook form and yup to verify inputs

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,68 +1,84 @@
 import { useState } from "react";
 
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import { useSelector, useDispatch } from "react-redux";
 
 import { addTransactionThunk } from "../../Store/Modules/listTransactions/thunks";
+import { Container } from "./styles";
 
 const Form = () => {
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [type, setType] = useState("");
   const list = useSelector((store) => store.list);
   const dispatch = useDispatch();
 
-  const createListTransition = () => {
+  const schema = yup.object().shape({
+    descricao: yup.string().required("Required Field"),
+    preco: yup.number("Please just numbers").required("Campo Obrigatório"),
+    tipo: yup.string().required("Required field"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const createListTransition = ({ descricao, preco, tipo }) => {
     let NewId;
-
     list.length < 1 ? (NewId = 1) : (NewId = list.length + 1);
-
-    const trasaction = {
-      id: NewId,
-      descricao: description,
-      preco: price,
-      tipo: type,
+    const transaction = {
+      NewId,
+      descricao,
+      preco,
+      tipo,
     };
-    dispatch(addTransactionThunk(trasaction));
+
+    dispatch(addTransactionThunk(transaction));
   };
   return (
-    <form
-      className="form--content"
-      onSubmit={(event) => {
-        createListTransition(event.preventDefault());
-      }}
-    >
-      <label>Description</label>
-      <input
-        className="description--input"
-        value={description}
-        onChange={(event) => setDescription(event.target.value)}
-        placeholder="Put a descritption"
-      />
-      <div className="price--content">
-        <label>
-          Value
-          <input
-            className="price--input"
-            value={price}
-            onChange={(event) => setPrice(event.target.value)}
-            placeholder="$"
-          />
-        </label>
-        <label>
-          Type of Value
-          <select
-            className="price--type"
-            value={type}
-            onChange={(event) => setType(event.target.value)}
-          >
-            <option value="" disabled selected></option>
-            <option value="Deposit">Deposit</option>
-            <option value="Output">Output</option>
-          </select>
-        </label>
-      </div>
-      <button>Send Trasaction</button>
-    </form>
+    <Container>
+      <form
+        className="form--content"
+        onSubmit={handleSubmit(createListTransition)}
+      >
+        <label>Description</label>
+        <input
+          className="description--input"
+          {...register("descricao")}
+          placeholder="Put a descritption"
+        />
+        {errors.descricao && (
+          <span className="error--mensage">{errors.descricao.message}</span>
+        )}
+        <div className="price--content">
+          <label>
+            Value
+            <input
+              className="price--input"
+              {...register("preco")}
+              placeholder="$"
+            />
+          </label>
+          {errors.descricao && (
+            <span className="error--mensage">{errors.descricao.message}</span>
+          )}
+          <label>
+            Type of Value
+            <select className="price--type" {...register("tipo")}>
+              <option value="" disabled selected></option>
+              <option value="Deposit">Deposit</option>
+              <option value="Output">Output</option>
+            </select>
+          </label>
+          {errors.tipo && (
+            <span className="error--mensage">{errors.tipo.message}</span>
+          )}
+        </div>
+        <button>Send Trasaction</button>
+      </form>
+    </Container>
   );
 };
 export default Form;
