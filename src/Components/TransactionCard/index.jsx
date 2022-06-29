60073fb2d597c32def9b0e2c3be9127b727ae66d
HenriqueMts <henrique.mts@outlook.com.br>
2022-06-29 14:22:54 -0300

    feat: implement remove transaction function at button

--- src/Components/TransactionCard/index.jsx
+++ src/Components/TransactionCard/index.jsx
@@ -1,13 +1,21 @@
+import { removeTransactionThunk } from "../../Store/Modules/listTransactions/thunks";
 import { Container, Content } from "./styles";
+import { useDispatch } from "react-redux";
 
 const TransactionCard = ({ transaction }) => {
+  const dispatch = useDispatch();
+
   return (
     <Container>
       <h2>{transaction.descricao}</h2>
       <p>{transaction.tipo}</p>
       <Content>
         <p>{transaction.preco}</p>
-        <button>x</button>
+        <button
+          onClick={() => dispatch(removeTransactionThunk(transaction.id))}
+        >
+          x
+        </button>
       </Content>
     </Container>
   );
